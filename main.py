a4c7cdb2245215ee820575390de7ff8a5d5525a0
mh3enn <135334114+mh3enn@users.noreply.github.com>
2026-02-05 11:38:34 +0330

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram.ext import (
 )
 from telegram.ext import filters as tg_filters
 
-from config import TOKEN, ADMIN_GROUP_ID
+from config import TOKEN, ADMIN_GROUP_ID,DATABASE_URL
 from db import init_db
 from handlers.start import start
 from handlers.admin import handle_admin_group_media
@@ -14,7 +14,7 @@ from handlers.callbacks import check_join_callback
 
 
 async def on_startup(app):
-    await init_db(app)
+    await init_db(app, DATABASE_URL)
 
 
 def error_handler(update, context):
@@ -44,3 +44,4 @@ app.add_error_handler(error_handler)
 
 if __name__ == "__main__":
     app.run_polling()
+
