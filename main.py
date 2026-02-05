191873f2a41de3e02558859171c49ce366e7f852
mh3enn <135334114+mh3enn@users.noreply.github.com>
2026-02-05 18:58:45 +0330

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import traceback
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -6,10 +7,10 @@ from telegram.ext import (
 )
 from telegram.ext import filters as tg_filters
 
-from config import TOKEN, ADMIN_GROUP_ID,DATABASE_URL
+from config import TOKEN, ADMIN_GROUP_ID, DATABASE_URL
 from db import init_db
 from handlers.start import start
-from handlers.admin import handle_admin_group_media,stats
+from handlers.admin import handle_admin_group_media, stats
 from handlers.callbacks import check_join_callback
 
 
@@ -18,29 +19,44 @@ async def on_startup(app):
 
 
 async def error_handler(update, context):
-    print("❌ Error:", context.error)
-
-app = (
-    ApplicationBuilder()
-    .token(TOKEN)
-    .post_init(on_startup)
-    .build()
-)
-
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("stats", stats))
-app.add_handler(
-    MessageHandler(
-        tg_filters.Chat(ADMIN_GROUP_ID) &
-        (tg_filters.VIDEO | tg_filters.Document.ALL),
-        handle_admin_group_media
+    print("❌ Exception occurred:")
+    traceback.print_exception(
+        type(context.error),
+        context.error,
+        context.error.__traceback__
     )
-)
-app.add_handler(
-    CallbackQueryHandler(check_join_callback, pattern=r"^(check_join:|no_link:)")
-)
 
-app.add_error_handler(error_handler)
+
+def main():
+    app = (
+        ApplicationBuilder()
+        .token(TOKEN)
+        .post_init(on_startup)
+        .build()
+    )
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("stats", stats))
+
+    app.add_handler(
+        MessageHandler(
+            tg_filters.Chat(ADMIN_GROUP_ID) &
+            (tg_filters.VIDEO | tg_filters.Document.ALL),
+            handle_admin_group_media
+        )
+    )
+
+    app.add_handler(
+        CallbackQueryHandler(
+            check_join_callback,
+            pattern=r"^(check_join:|no_link:)"
+        )
+    )
+
+    app.add_error_handler(error_handler)
+
+    app.run_polling()
+
 
 if __name__ == "__main__":
-    app.run_polling()
+    main()
