6ad62964897fede15f01a2ca73f1ccaf147f1fda
mh3enn <135334114+mh3enn@users.noreply.github.com>
2026-02-06 22:49:44 +0330

    Update get_session.py

--- get_session.py
+++ get_session.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-API_ID = 123456
-API_HASH = "API_HASH_HERE"
+API_ID = 35198332
+API_HASH = "236ab0a7906c073f9f891c70670c7d39"
 
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     print("\nSESSION_STRING:\n")
